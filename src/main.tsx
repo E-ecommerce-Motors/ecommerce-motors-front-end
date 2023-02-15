b693428c6366f53b3d5a49a0087aa82647face51
Galvs135 <lg_franca@hotmail.com>
2023-02-15 01:54:41 -0300

    feat: createGlobalStyle

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MyThemeProvider } from "./styles/MyThemeProvider";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <MyThemeProvider></MyThemeProvider>
+  </React.StrictMode>
+);
