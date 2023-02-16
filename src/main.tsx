52e123751aa9db21c420ac26c3a2098b50b026d0
Galvs135 <lg_franca@hotmail.com>
2023-02-16 04:10:09 -0300

    fix: deleted app created for testing button

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./pages/app";
 import { MyThemeProvider } from "./styles/MyThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MyThemeProvider>
-      <App />
-    </MyThemeProvider>
+    <MyThemeProvider></MyThemeProvider>
   </React.StrictMode>
 );
