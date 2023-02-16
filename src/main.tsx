47e43c4ad58c92e85303418217209919b3d55a75
Galvs135 <lg_franca@hotmail.com>
2023-02-16 04:04:26 -0300

    feat: created product card

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Button } from "./pages/Button";
-import { Card } from "./pages/Card";
 import { MyThemeProvider } from "./styles/MyThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MyThemeProvider>
-      <Card />
-      <Button />
-    </MyThemeProvider>
+    <MyThemeProvider></MyThemeProvider>
   </React.StrictMode>
 );
