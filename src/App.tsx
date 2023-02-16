b04c055405b0abce71f136a98d3ce2fc83ae6728
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2023-02-15 21:03:35 -0300

    feat: style of the TextAreaInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { ToastContainer } from "react-toastify";
 import DashboardFooter from "./shared/components/DashboardFooter";
+import TextAreaInput from "./shared/components/TextAreaInput";
 
 const App = () => {
   return (
     <>
-      <DashboardFooter></DashboardFooter>
       <ToastContainer
         position={"top-right"}
         autoClose={1000}
