127f2f1eca82b90b3a04f5452a3d0a2b978501e7
Galvs135 <lg_franca@hotmail.com>
2023-02-17 02:03:01 -0300

    feat: created product card auction

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Button } from "./pages/Button";
-import { Card } from "./pages/Card";
-import { CardAuction } from "./pages/CardAuction";
 import { MyThemeProvider } from "./styles/MyThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MyThemeProvider>
-      {/* <Card />
-      <Button /> */}
-      <CardAuction />
-    </MyThemeProvider>
+    <MyThemeProvider></MyThemeProvider>
   </React.StrictMode>
 );
