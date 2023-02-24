0fb5732d08c50a9e63446a0ee3875277a977ca4d
Galvs135 <lg_franca@hotmail.com>
2023-02-24 06:52:04 -0300

    feat: created carousel for auction and default

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,17 @@
+import { useEffect, useState } from "react";
+import { Carousel } from "../../shared/components/carousel";
+import { CarouselAuction } from "../../shared/components/carouselAuction";
 import Footer from "../../shared/components/Footer";
-import { NavBar } from "../../shared/components/NavBar";
+import { NavBar } from "../../shared/components/navBar";
+import { api } from "../../shared/services/api";
 
 export const Dashboard = () => {
   return (
     <>
       <NavBar auth={"default"} user={"Lucas Galvs"} />
+      <CarouselAuction />
+      <Carousel type={"car"} />
+      <Carousel type={"motorcycle"} />
       <Footer />
     </>
   );
