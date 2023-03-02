41816de741e651f51d7e5dfad3558516f6302af6
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-03-02 00:34:58 -0300

    feat: Register form and styles

--- src/shared/components/RegisterForm/index.tsx
+++ src/shared/components/RegisterForm/index.tsx
@@ -1,61 +1,121 @@
+import { theme } from "../../../styles/theme";
 import { Title } from "../Carousel/styles";
-import { TextArea } from "../Modal/editAnnouncement/styles";
-import { ContentAdress, ContentForm, Input, SubTitle } from "./styles";
+import IconButton from "@mui/material/IconButton";
+import {
+  ContentAdress,
+  ContentButton,
+  ContentForm,
+  Div,
+  Input,
+  SubTitle,
+  TextArea,
+} from "./styles";
 
 export const RegisterForm = () => {
   return (
     <ContentForm>
-      <Title>Cadastro</Title>
-      <h4>Informações pessoais</h4>
-      <SubTitle>Nome</SubTitle>
-      <Input placeholder="Ex: Samuel Leão" width={"big"} />{" "}
-      <SubTitle>Email</SubTitle>
-      <Input placeholder="Ex: Samuel Leão" width={"big"} />{" "}
-      <SubTitle>CPF</SubTitle>
-      <Input placeholder="Ex: Samuel Leão" width={"big"} />{" "}
-      <SubTitle>Celular</SubTitle>
-      <Input placeholder="Ex: Samuel Leão" width={"big"} />{" "}
-      <SubTitle>Data de nascimento</SubTitle>
-      <Input placeholder="Ex: Samuel Leão" width={"big"} />{" "}
-      <SubTitle>Descrição</SubTitle>
-      {/* <TextArea /> */}
+      <div>
+        <Title>Cadastro</Title>
+        <h4>Informações pessoais</h4>
+        <SubTitle>Nome</SubTitle>
+        <Input placeholder="Ex: Samuel Leão" />
+        <SubTitle>Email</SubTitle>
+        <Input placeholder="Ex: samuel@kenzie.com.br" />
+        <SubTitle>CPF</SubTitle>
+        <Input placeholder="000.000.000-00" />
+        <SubTitle>Celular</SubTitle>
+        <Input placeholder="(DDD) 90000-0000" />
+        <SubTitle>Data de nascimento</SubTitle>
+        <Input placeholder="00/00/00" />
+        <SubTitle>Descrição</SubTitle>
+        <TextArea placeholder="Digitar descrição" />
+      </div>
       <div>
         <h4>Informações de endereço</h4>
         <SubTitle>CEP</SubTitle>
-        <Input placeholder="Ex: Samuel Leão" width={"big"} />
+        <Input placeholder="00000.000" />
         <ContentAdress>
-          <div>
+          <Div>
             <SubTitle>Estado</SubTitle>
-            <Input placeholder="Digitar estado" width={"none"} />{" "}
-          </div>
-          <div>
+            <Input placeholder="Digitar estado" />
+          </Div>
+          <Div>
             <SubTitle>Cidade</SubTitle>
-            <Input placeholder="Digitar cidade" width={"none"} />{" "}
-          </div>
+            <Input placeholder="Digitar cidade" />
+          </Div>
         </ContentAdress>
         <SubTitle>Rua</SubTitle>
-        <Input placeholder="Ex: Samuel Leão" width={"big"} />{" "}
+        <Input placeholder="Digitar rua" />
         <ContentAdress>
           <div>
             <SubTitle>Número</SubTitle>
-            <Input placeholder="Ex: Samuel Leão" width={"none"} />{" "}
+            <Input placeholder="Digitar número" />
           </div>
           <div>
             <SubTitle>Complemento</SubTitle>
-            <Input placeholder="Ex: Samuel Leão" width={"none"} />{" "}
+            <Input placeholder="Ex: apart 307" />
           </div>
         </ContentAdress>
+        <SubTitle>Tipo de conta</SubTitle>
+        <ContentButton>
+          <IconButton
+            sx={{
+              borderRadius: 1,
+              height: 48,
+              width: 152,
+              border: `2px solid${theme.colors.grey4}`,
+              fontSize: `${theme.size.button_big_text}`,
+              fontWeight: `${theme.weight.Heading_2_600}`,
+              color: `${theme.colors.grey0}`,
+
+              "&:hover": {
+                backgroundColor: `${theme.colors.brand1}`,
+                border: `2px solid${theme.colors.brand1}`,
+                color: `${theme.colors.whiteFixed}`,
+              },
+            }}
+          >
+            Comprador
+          </IconButton>
+          <IconButton
+            sx={{
+              borderRadius: 1,
+              height: 48,
+              width: 152,
+              border: `2px solid${theme.colors.grey4}`,
+              fontSize: `${theme.size.button_big_text}`,
+              fontWeight: `${theme.weight.Heading_2_600}`,
+              color: `${theme.colors.grey0}`,
+
+              "&:hover": {
+                backgroundColor: `${theme.colors.brand1}`,
+                border: `2px solid${theme.colors.brand1}`,
+                color: `${theme.colors.whiteFixed}`,
+              },
+            }}
+          >
+            Anunciante
+          </IconButton>
+        </ContentButton>
+        <SubTitle>Senha</SubTitle>
+        <Input placeholder="Digitar senha" />
+        <SubTitle>Confirmar Senha</SubTitle>
+        <Input placeholder="Confirmar senha" />
+        <IconButton
+          sx={{
+            borderRadius: 1,
+            height: 48,
+            backgroundColor: `${theme.colors.brand1}`,
+            color: `${theme.colors.whiteFixed}`,
+            fontSize: `${theme.size.button_big_text}`,
+            "&:hover": {
+              backgroundColor: `${theme.colors.brand1}`,
+            },
+          }}
+        >
+          Finalizar Cadastro
+        </IconButton>
       </div>
     </ContentForm>
   );
 };
-
-//  <Input
-//    width={"big"}
-//    value={e}
-//    placeholder={e}
-//    onChange={(element) => {
-//      setChange(false);
-//      handleIncrementClick(element.target.value, index);
-//    }}
-//  />;, Input
