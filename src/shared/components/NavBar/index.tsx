b958f5d31034cb8a6ec543b06c382c036e157ded
Galvs135 <lg_franca@hotmail.com>
2023-03-03 14:01:52 -0300

    fix: alter optionProfile for buyer User

--- src/shared/components/NavBar/index.tsx
+++ src/shared/components/NavBar/index.tsx
@@ -76,7 +76,11 @@ export const NavBar = () => {
               >
                 <OptionsProfile>Editar Perfil</OptionsProfile>
                 <OptionsProfile>Editar Endereço</OptionsProfile>
-                <OptionsProfile>Minhas Compras</OptionsProfile>
+                <OptionsProfile>
+                  {userData.typeAccount == "advertiser"
+                    ? "Meus Anúncios"
+                    : "Minhas Compras"}
+                </OptionsProfile>
                 <OptionsProfile onClick={() => logout()}>Sair</OptionsProfile>
               </MenuBox>
             </Profile>
